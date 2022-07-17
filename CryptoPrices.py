1d5632606088b1eb12e36c50a70cc26e97ba239d
Abhishek Tiwari <34565942+abhishek4563@users.noreply.github.com>
2022-07-17 20:08:40 +0100

    first file
    creating the first file in the project to play around with getting and visualising real crypto data via an API

---
+++ CryptoPrices.py
@@ -0,0 +1,25 @@
+# set up a vairable to connect and get API data 
+
+
+#get current bitcoin data 
+
+
+# get current eth data 
+
+
+# display a chart for last 24 hours btc data 
+
+
+# display a chart for last 24 hours eth data 
+
+
+#show the live btc price across exchanges in 1 window 
+
+
+#show the live eth price across exchanges in 1 window 
+
+#todos
+# best practices for connecting to an API
+#best practices for token management 
+# basic storage and visualisation of data 
+#creating a live dashboard in python 
